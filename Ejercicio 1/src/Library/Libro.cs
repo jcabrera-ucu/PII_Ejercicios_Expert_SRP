544f638d7aba1fe439f17fa9bbfa579cd2b846eb
jcabrera-ucu <101676632+jcabrera-ucu@users.noreply.github.com>
2022-04-05 20:20:26 -0300

    Crítica 2

--- Ejercicio 1/src/Library/Libro.cs
+++ Ejercicio 1/src/Library/Libro.cs
@@ -3,7 +3,8 @@
 namespace SRP
 {
     // Esta clase no cumple con SRP ya que además de tener la responsabilidad
-    // de conocer los datos propios del libro, también conoce dónde se encuentra.
+    // de conocer los datos propios del libro, también conoce dónde se encuentra y
+    // tiene un método para AlmacenarLibro.
     public class Libro
     {
 
